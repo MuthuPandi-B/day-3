78955e0e91f5440bf6c77705f43749140f30b9b0
MuthuPandi-B <muthup61@gmail.com>
2024-08-06 17:44:25 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function medean (arr1,arr2){
+    let sum=(arr1.concat(arr2));
+    let sum1=sum.sort();
+    let n=sum1.length;
+    let i,j,result
+    
+        if(n%2!==0){
+           i=n/2;
+            j=Math.ceil(i);
+             result=(sum1[j]);
+        }
+     
+      else
+        {
+        i=n/2;
+       j=i+1;
+        result=(sum1[i]+sum1[j]/2)
+        }
+    console.log(result);
+        
+    }
+    medean([3,6,8,9],[1,2,3,4])
